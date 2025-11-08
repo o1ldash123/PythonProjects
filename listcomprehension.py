d1f7cbcba87477a2d896d5304c66a8b146909c87
o1ldash123 <adekunbibabayomi@gmail.com>
2025-11-07 20:57:15 -0600

    LISTCOMPRE

---
+++ listcomprehension.py
@@ -0,0 +1,18 @@
+print('this program counts the number of  what ever letter u choose in a word example : h in hello = 1 ')
+letter = input('pick a letter ')
+word = input('choose a word ')
+if  letter.upper() in  {'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z'}  :
+  print('it is ')
+else :
+  print('invalid letters try picking a letter next time')
+  exit()
+  
+
+def calc(words , letters ) :
+  result = [i  for i in words if i.upper() == letters.upper()]
+  return len(result)
+  
+
+
+print('The numbers of {}`s in {} is {}'.format(letter , word ,  calc(word , letter)))
+
